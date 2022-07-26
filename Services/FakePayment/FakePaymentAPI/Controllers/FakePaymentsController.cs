d6e4e04809e2f493b4be562e06e7da81c0badda3
abdulhamit-dev <hmd62392@gmail.com>
2022-07-26 23:31:07 +0300

    FakePayment Ödeme için basit API

---
+++ Services/FakePayment/FakePaymentAPI/Controllers/FakePaymentsController.cs
@@ -0,0 +1,23 @@
+﻿using FakePaymentAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.ControllerBases;
+using Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FakePaymentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymentsController : CustomBaseController
+    {
+        [HttpPost]
+        public IActionResult ReceivePayment()
+        {
+            return CreateActionResultInstance(Response<NoContent>.Success(200));
+        }
+    }
+}
