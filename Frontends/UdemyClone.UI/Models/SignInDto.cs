f029a2f0ef3809c21ca1cfbdd556021dffb8287f
abdulhamit-dev <hmd62392@gmail.com>
2023-04-03 23:17:06 +0300

    UdemyClone UI add

---
+++ Frontends/UdemyClone.UI/Models/SignInDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace UdemyClone.UI.Models
+{
+    public class SignInDto
+    {
+        [DisplayName("Email Adresiniz")]
+        public string Email { get; set; }
+        [DisplayName("Şifreniz")]
+        public string Password { get; set; }
+        [DisplayName("Beni Hatırla")]
+        public bool IsRemember { get; set; }
+    }
+}
---
+++ Frontends/UdemyClone.UI/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,14 @@
+﻿using IdentityModel.Client;
+using Shared.Dtos;
+using System.Threading.Tasks;
+using UdemyClone.UI.Models;
+
+namespace UdemyClone.UI.Services.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<Response<bool>> SignIn(SignInDto signInDto);
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+        Task RevokeRefreshToken();
+    }
+}
