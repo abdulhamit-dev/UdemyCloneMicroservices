892e1a759e46ff8978926a56d3431bb6fa3a0546
abdulhamit-dev <hmd62392@gmail.com>
2023-04-04 23:46:59 +0300

    Client and Service settings

--- Frontends/UdemyClone.UI/Startup.cs
+++ Frontends/UdemyClone.UI/Startup.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UdemyClone.UI.Models;
 
 namespace UdemyClone.UI
 {
@@ -22,6 +23,8 @@ namespace UdemyClone.UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ServiceApiSettings>(Configuration.GetSection("ServiceApiSettings"));
+            services.Configure<ClientSettings>(Configuration.GetSection("ClientSettings"));
             services.AddControllersWithViews();
         }
 
