bda51a6c8efa8199d9782d069dd725ebcaa06ec1
abdulhamit <hmd62392@gmail.com>
2023-05-01 01:16:54 +0300

    UnAuthorizeException

--- Frontends/UdemyClone.UI/Controllers/HomeController.cs
+++ Frontends/UdemyClone.UI/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using UdemyClone.UI.Exceptions;
 using UdemyClone.UI.Models;
 using UdemyClone.UI.Services.Interfaces;
 
@@ -35,6 +37,19 @@ namespace UdemyClone.UI.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            var errorFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            if (errorFeature != null && errorFeature.Error is UnAuthorizeException)
+            {
+                return RedirectToAction(nameof(AuthController.Logout), "Auth");
+            }
+
+            return View(new ErrorVM { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
         
---
+++ Frontends/UdemyClone.UI/Models/ErrorVM.cs
@@ -0,0 +1,8 @@
+namespace UdemyClone.UI.Models;
+
+public class ErrorVM
+{
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
